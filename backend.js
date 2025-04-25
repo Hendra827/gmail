cf41395b8f0d5f1737e71e7be64b8dd6825e0e9f
Hendra827 <31788233+Hendra827@users.noreply.github.com>
2025-04-25 09:43:20 +0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4761;
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
